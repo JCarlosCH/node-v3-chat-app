bc59f958d950283ca5f4d49d3cc454cd15160808
Juan C. Castellanos <jncarlosch@gmail.com>
2022-12-21 10:14:56 -0800

    Sets up socket.io

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,9 @@
+import { createServer } from "node:http";
 import path from "node:path";
 import * as url from "node:url";
 
 import express from "express";
+import { Server } from "socket.io";
 
 const { PORT } = process.env;
 
@@ -9,9 +11,15 @@ const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(PORT, () => {
+io.on("connection", () => {
+    console.log("New WebSocket connection");
+});
+
+server.listen(PORT, () => {
     console.log(`Server is up on port ${PORT}`);
 });
